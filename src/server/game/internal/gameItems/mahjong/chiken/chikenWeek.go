573b29de6fb80b839238079634625e180941350f
chenzhihua <po2656233@qq.com>
2020-02-05 22:06:51 +0800

    删除原有

--- src/server/game/internal/gameItems/mahjong/chiken/chikenWeek.go
+++
@@ -1,6 +0,0 @@
-package chiken
-
-// GetWeekChikens 获取星期鸡
-func (mc *MChiken) GetWeekChikens() []int {
-	return []int{mc.weekDay, mc.weekDay + 10, mc.weekDay + 20}
-}
--- src/server/game/internal/gameItems/mahjong/chiken/self.go
+++
@@ -1,6 +0,0 @@
-package chiken
-
-// GetChikenSelf 获取本鸡
-func (mc *MChiken) GetChikenSelf() int {
-	return mc.GetChikenDraw()
-}
