6f536b2ed810903a5bd1cc6a8716a90ba28f0201
po2656233 <po2656233@qq.com>
2020-11-05 16:05:38 +0800

    1111

--- miniRobot/game/internal/handler.go
+++ miniRobot/game/internal/handler.go
@@ -20,6 +20,8 @@ func init() {
     handlerMsg(&protoMsg.EnterGameMJResp{}, handleEnterGameMJ)     //反馈--->主页信息
     handlerMsg(&protoMsg.EnterGameMJERResp{}, handleEnterGameMJER) //反馈--->主页信息
     handlerMsg(&protoMsg.EnterGameSGResp{}, handleEnterGameSG)     //反馈--->主页信息
+    handlerMsg(&protoMsg.LandLordsSceneResp{}, handleEnterGameLandLords)     //反馈--->主页信息
+
 
     //
     handlerMsg(&protoMsg.QzcowcowStateFreeResp{}, handleQzcowcowStateFreeResp)   //反馈--->主页信息
@@ -28,6 +30,7 @@ func init() {
     handlerMsg(&protoMsg.MahjongERStateFreeResp{}, handleMahjongERStateFreeResp) //反馈--->主页信息
     handlerMsg(&protoMsg.ZhajinhuaStateFreeResp{}, handleZhajinhuaStateFreeResp) //反馈--->主页信息
     handlerMsg(&protoMsg.SangongStateFreeResp{}, handleSangongStateFreeResp)     //反馈--->主页信息
+    handlerMsg(&protoMsg.LandLordsStateFreeResp{}, handleLandLordsStateFreeResp) //反馈--->主页信息
 
     //
     handlerMsg(&protoMsg.BaccaratStatePlayingResp{}, handleBaccaratStatePlayingResp)         //反馈--->主页信息
@@ -36,6 +39,16 @@ func init() {
     handlerMsg(&protoMsg.TuitongziStatePlayingResp{}, handleTuitongziStatePlayingResp)       //反馈--->主页信息
     handlerMsg(&protoMsg.TigerXdragonStatePlayingResp{}, handleTigerXdragonStatePlayingResp) //反馈--->主页信息
 
+    ///对战出牌
+    handlerMsg(&protoMsg.ZhajinhuaStatePlayingResp{}, handleZhajinhuaStatePlayingResp) //反馈--->主页信息
+    handlerMsg(&protoMsg.MahjongStatePlayingResp{}, handleMahjongStatePlayingResp) //反馈--->主页信息
+    handlerMsg(&protoMsg.MahjongERStatePlayingResp{}, handleMahjongERStatePlayingResp) //反馈--->主页信息
+
+
+    //操作提示
+    handlerMsg(&protoMsg.MahjongHintResp{}, handleMahjongHintResp) //反馈--->主页信息
+    handlerMsg(&protoMsg.MahjongERHintResp{}, handleMahjongERHintResp) //反馈--->主页信息
+
 }
 
 //注册传输消息
@@ -43,6 +56,7 @@ func handlerMsg(m interface{}, h interface{}) {
     skeleton.RegisterChanRPC(reflect.TypeOf(m), h)
 }
 
+///////////////////////入场/////////////////////////////
 //百人类
 func handleEnterGame(args []interface{}) {
     m := args[0].(*protoMsg.EnterGameResp)
@@ -140,7 +154,18 @@ func handleEnterGameSG(args []interface{}) {
 
 }
 
-////////////////////////
+func handleEnterGameLandLords(args []interface{}) {
+    m := args[0].(*protoMsg.LandLordsSceneResp)
+    a := args[1].(gate.Agent)
+    //person := a.UserData().(*protoMsg.UserInfo)
+    log.Debug("进入游戏:%v", m)
+    msg := &protoMsg.LandLordsReadyReq{
+        IsReady: true,
+    }
+    a.WriteMsg(msg)
+
+}
+//////////////////////准备///////////////////////////////////
 func handleQzcowcowStateFreeResp(args []interface{}) {
     m := args[0].(*protoMsg.QzcowcowStateFreeResp)
     a := args[1].(gate.Agent)
@@ -214,18 +239,121 @@ func handleSangongStateFreeResp(args []interface{}) {
     })
 
 }
+func handleLandLordsStateFreeResp(args []interface{}) {
+    m := args[0].(*protoMsg.LandLordsStateFreeResp)
+    a := args[1].(gate.Agent)
+    second, _ := rand.Int(rand.Reader, big.NewInt(int64(m.Times.TotalTime)))
+    time.AfterFunc(time.Duration(second.Int64())*time.Second, func() {
+        msg := &protoMsg.LandLordsReadyReq{
+            IsReady: true,
+        }
+        a.WriteMsg(msg)
+    })
+}
 
-/////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////
+//////////////////////////对战类/////////////////////////////////////////////
+func handleZhajinhuaStatePlayingResp(args []interface{}) {
+    m := args[0].(*protoMsg.ZhajinhuaStatePlayingResp)
+    a := args[1].(gate.Agent)
+    person := a.UserData().(*protoMsg.UserInfo)
+    if person.UserID == m.UserID {
+        second, _ := rand.Int(rand.Reader, big.NewInt(int64(m.Times.TotalTime)))
+        time.AfterFunc(time.Duration(second.Int64())*time.Second, func() {
+            switch second.Int64() % 3 {
+            case 0:
+                msg := &protoMsg.ZhajinhuaFollowReq{}
+                a.WriteMsg(msg)
+            case 1:
+                scoreR, _ := rand.Int(rand.Reader, big.NewInt(100))
+                score := scoreR.Int64() * 100
+                msg := &protoMsg.ZhajinhuaRaiseReq{
+                    Score: score,
+                }
+                a.WriteMsg(msg)
+            case 2:
+                msg := &protoMsg.ZhajinhuaGiveupReq{}
+                a.WriteMsg(msg)
+            }
+        })
+    }
+}
+
+func handleMahjongStatePlayingResp(args []interface{}) {
+    m := args[0].(*protoMsg.MahjongStatePlayingResp)
+    a := args[1].(gate.Agent)
+    person := a.UserData().(*protoMsg.UserInfo)
+    if person.UserID == m.UserID {
+        second, _ := rand.Int(rand.Reader, big.NewInt(int64(m.Times.TotalTime)))
+        time.AfterFunc(time.Duration(second.Int64())*time.Second, func() {
+            msg := &protoMsg.MahjongOutCardReq{
+                Card: m.Card,
+            }
+            a.WriteMsg(msg)
+        })
+    }
+}
+
+func handleMahjongERStatePlayingResp(args []interface{}) {
+    m := args[0].(*protoMsg.MahjongERStatePlayingResp)
+    a := args[1].(gate.Agent)
+    person := a.UserData().(*protoMsg.UserInfo)
+    if person.UserID == m.UserID {
+        second, _ := rand.Int(rand.Reader, big.NewInt(int64(m.Times.TotalTime)))
+        time.AfterFunc(time.Duration(second.Int64())*time.Second, func() {
+            msg := &protoMsg.MahjongEROutCardReq{
+                Card: m.Card,
+            }
+            a.WriteMsg(msg)
+        })
+    }
+}
+
+
+//////////////////操作///////////////////////////////////////////
+func handleMahjongHintResp(args []interface{}) {
+    m := args[0].(*protoMsg.MahjongHintResp)
+    a := args[1].(gate.Agent)
+    person := a.UserData().(*protoMsg.UserInfo)
+    if person.UserID == m.UserID {
+        size:=len(m.Hints)
+        one,_:=rand.Int(rand.Reader, big.NewInt(int64(size)))
+        index:=one.Int64()
+        msg := &protoMsg.MahjongOperateReq{
+            Code: m.Hints[int(index)].Code,
+            Cards: m.Hints[int(index)].Cards,
+        }
+        a.WriteMsg(msg)
+    }
+}
+func handleMahjongERHintResp(args []interface{}) {
+    m := args[0].(*protoMsg.MahjongERHintResp)
+    a := args[1].(gate.Agent)
+    person := a.UserData().(*protoMsg.UserInfo)
+    if person.UserID == m.UserID {
+        size:=len(m.Hints)
+        one,_:=rand.Int(rand.Reader, big.NewInt(int64(size)))
+        index:=one.Int64()
+        msg := &protoMsg.MahjongEROperateReq{
+            Code: m.Hints[int(index)].Code,
+            Cards: m.Hints[int(index)].Cards,
+        }
+        a.WriteMsg(msg)
+    }
+}
+
+
+
+
+/////////////////////////百人类////////////////////////////////
 func handleBaccaratStatePlayingResp(args []interface{}) {
     m := args[0].(*protoMsg.BaccaratStatePlayingResp)
     a := args[1].(gate.Agent)
     //	person := a.UserData().(*protoMsg.UserInfo)
     secondR, _ := rand.Int(rand.Reader, big.NewInt(int64(m.Times.TotalTime)))
-    areaR,_:=rand.Int(rand.Reader, big.NewInt(8))
+    areaR, _ := rand.Int(rand.Reader, big.NewInt(8))
     area := int32(areaR.Int64())
-    scoreR,_:=rand.Int(rand.Reader, big.NewInt(100))
-    score := scoreR.Int64()*100
+    scoreR, _ := rand.Int(rand.Reader, big.NewInt(100))
+    score := scoreR.Int64() * 100
     time.AfterFunc(time.Duration(secondR.Int64())*time.Second, func() {
         msg := &protoMsg.BaccaratBetReq{
             BetArea:  area,
@@ -239,10 +367,10 @@ func handleBrcowcowStatePlayingResp(args []interface{}) {
     a := args[1].(gate.Agent)
     // person := a.UserData().(*protoMsg.UserInfo)
     secondR, _ := rand.Int(rand.Reader, big.NewInt(int64(m.Times.TotalTime)))
-    areaR,_:=rand.Int(rand.Reader, big.NewInt(4))
+    areaR, _ := rand.Int(rand.Reader, big.NewInt(4))
     area := int32(areaR.Int64())
-    scoreR,_:=rand.Int(rand.Reader, big.NewInt(100))
-    score := scoreR.Int64()*100
+    scoreR, _ := rand.Int(rand.Reader, big.NewInt(100))
+    score := scoreR.Int64() * 100
     time.AfterFunc(time.Duration(secondR.Int64())*time.Second, func() {
         msg := &protoMsg.BrcowcowBetReq{
             BetArea:  area,
@@ -256,10 +384,10 @@ func handleBrtoubaoStatePlayingResp(args []interface{}) {
     a := args[1].(gate.Agent)
     //  person := a.UserData().(*protoMsg.UserInfo)
     secondR, _ := rand.Int(rand.Reader, big.NewInt(int64(m.Times.TotalTime)))
-    areaR,_:=rand.Int(rand.Reader, big.NewInt(25))
+    areaR, _ := rand.Int(rand.Reader, big.NewInt(25))
     area := int32(areaR.Int64())
-    scoreR,_:=rand.Int(rand.Reader, big.NewInt(100))
-    score := scoreR.Int64()*100
+    scoreR, _ := rand.Int(rand.Reader, big.NewInt(100))
+    score := scoreR.Int64() * 100
     time.AfterFunc(time.Duration(secondR.Int64())*time.Second, func() {
         msg := &protoMsg.BrtoubaoBetReq{
             BetArea:  area,
@@ -273,10 +401,10 @@ func handleTuitongziStatePlayingResp(args []interface{}) {
     a := args[1].(gate.Agent)
     // person := a.UserData().(*protoMsg.UserInfo)
     secondR, _ := rand.Int(rand.Reader, big.NewInt(int64(m.Times.TotalTime)))
-    areaR,_:=rand.Int(rand.Reader, big.NewInt(3))
+    areaR, _ := rand.Int(rand.Reader, big.NewInt(3))
     area := int32(areaR.Int64())
-    scoreR,_:=rand.Int(rand.Reader, big.NewInt(100))
-    score := scoreR.Int64()*100
+    scoreR, _ := rand.Int(rand.Reader, big.NewInt(100))
+    score := scoreR.Int64() * 100
     time.AfterFunc(time.Duration(secondR.Int64())*time.Second, func() {
         msg := &protoMsg.TuitongziBetReq{
             BetArea:  area,
@@ -290,10 +418,10 @@ func handleTigerXdragonStatePlayingResp(args []interface{}) {
     m := args[0].(*protoMsg.TigerXdragonStatePlayingResp)
     a := args[1].(gate.Agent)
     secondR, _ := rand.Int(rand.Reader, big.NewInt(int64(m.Times.TotalTime)))
-    areaR,_:=rand.Int(rand.Reader, big.NewInt(13))
+    areaR, _ := rand.Int(rand.Reader, big.NewInt(13))
     area := int32(areaR.Int64())
-    scoreR,_:=rand.Int(rand.Reader, big.NewInt(100))
-    score := scoreR.Int64()*100
+    scoreR, _ := rand.Int(rand.Reader, big.NewInt(100))
+    score := scoreR.Int64() * 100
     time.AfterFunc(time.Duration(secondR.Int64())*time.Second, func() {
         msg := &protoMsg.TigerXdragonBetReq{
             BetArea:  area,
